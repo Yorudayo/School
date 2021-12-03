351b749b9d91f6bb3aef23f0de712ea26ab7d1ec
Yorudayo <83815054+Yorudayo@users.noreply.github.com>
2021-12-03 19:54:10 +0900

    2021秋第8回②

--- app/src/main/java/android/lifeistech/school2/ui/home/HomeFragment.kt
+++ app/src/main/java/android/lifeistech/school2/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package android.lifeistech.school2.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,19 +10,27 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import android.lifeistech.school2.R
+import android.lifeistech.school2.ui.WriteActivity
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_study, container, false)
         return root
     }
+
+
+    FAB.setOnClickListener{
+        val intent= Intent(this, WriteActivity::class.java)
+        startActivity(intent)
+
+    }
+
 }
