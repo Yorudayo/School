dd3d4667dd8ed8887811032bbcadc785e3515e59
Yorudayo <83815054+Yorudayo@users.noreply.github.com>
2021-12-10 20:15:30 +0900

    2021秋第9回

--- app/src/main/java/android/lifeistech/school2/MainActivity.kt
+++ app/src/main/java/android/lifeistech/school2/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
-//class MainActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 //        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 //
 //        val navController = findNavController(R.id.nav_host_fragment)
@@ -22,5 +22,5 @@ import androidx.navigation.ui.setupWithNavController
 //                R.id.navigation_study, R.id.navigation_home, R.id.navigation_break))
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
-//    }
-//}
+    }
+}
